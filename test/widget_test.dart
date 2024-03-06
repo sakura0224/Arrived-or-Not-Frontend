ef4dc703dc204b73e230d7ac9a9fba5f6d56efe4
sakura0224 <shiinayane@gmail.com>
2024-03-06 14:02:35 +0800

    fix the issue of changing the project name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:roll/main.dart';
+import 'package:daomeidao/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
